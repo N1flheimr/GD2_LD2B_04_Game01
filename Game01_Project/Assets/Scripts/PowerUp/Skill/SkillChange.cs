e84ebb159f91d3c7c49de797fc5350cd22eab4a8
N1flheimr <ryanwj1234@live.com>
2022-10-17 04:56:43 +0900

    created CharacterSkill class
    on InputManager added a new input button for player skill (q button), added testing for player skill, created scriptable object SkillChange script for changing current skill from CharacterSkill() class and created empty Skill class

---
+++ Game01_Project/Assets/Scripts/PowerUp/Skill/SkillChange.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MoreMountains.TopDownEngine
+{
+    [CreateAssetMenu(menuName = "PowerUpSystem/Weapon")]
+    public class SkillChange : PowerUp
+    {
+        [SerializeField] private Skill newSkill;
+        public override void Apply(Transform targetTransform)
+        {
+            CharacterSkill characterSkill = targetTransform.GetComponent<CharacterSkill>();
+            characterSkill.ChangeSkill(newSkill);
+        }
+    }
+}
